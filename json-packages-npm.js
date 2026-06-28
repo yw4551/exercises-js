7bac646e86cf82ea578923dcd50425ac2d38b9f4
Yitzi Weiner <yitziw123@gmail.com>
2026-06-28 08:33:07 +0300

    Added json-packages-npm.js

---
+++ json-packages-npm.js
@@ -0,0 +1,102 @@
+// Ex 1
+// CommonJS
+
+// ESModules
+
+// ESModules
+
+// CommonJS
+
+// Ex 2
+// import { add } from "./math.js";
+
+// {
+// "name": "my-app",
+// "type": "module"
+// }
+
+// import { fs } from "fs";
+
+// import path from "path";
+
+// Ex 3
+//{
+//    "name": "student-system",
+//    "version": "1.0.0",
+//        "scripts": {
+//        "dev": "nodemon index.js"
+//    },
+//    "dependencies": {
+//        "express": "^4.21.0"
+//    },
+//    "devDependencies": {
+//        "nodemon": "^3.1.4"
+//    }
+//}
+
+// Ex 4
+// dependencies
+
+// devDependencies
+
+// devDependencies
+
+// dependencies
+
+// devDependencies
+
+// Ex 5
+// 5 - MAJOR;
+// 18 - MINOR;
+// 3 - PATCH;
+
+// Ex 6
+// MINOR, PATCH
+// PATCH
+// None
+// All
+
+// Ex 7
+// No
+// Yes
+// Yes
+// No
+
+// Ex 8
+// When the code is running and hits that line
+// It can solve memory issues
+// It gives you more memory space
+
+// Ex 9
+// require | import
+// exports | export
+// Synchronous | Asynchronous
+// True | False
+
+// Ex 10
+// ESModules
+// Because it work with the Asynchronous system
+// it does not need a translation to work in modern browsers
+
+// Ex 11
+// Check package-lock.json
+// Read package.jason
+// Download packages
+// Lock versions
+
+// Ex 12
+// You are using a combination of commonJS and ESModules
+
+// Ex 13
+
+// Ex 14
+// To handel packages
+// All variables where available from all pages and it caused a lot of issues.
+// Dependencies - used modules. DevDependencies - build modules needed for development
+// It has all the version history
+// It is very heavy
+
+// Ex 15
+// SyntaxError, ERR_MODULE_NOT_FOUND
+// To change for one module
+// Use ESModule
