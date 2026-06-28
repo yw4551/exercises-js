4b98a4088ff6667b0366870af819ad1cb1659f85
Yitzi Weiner <yitziw123@gmail.com>
2026-06-28 11:03:24 +0300

    Added git-basic.js

---
+++ git-basic.js
@@ -0,0 +1,36 @@
+// Ex 1
+// 1. A directory is a folder located on the local machine and a repository is a the folder on the cloud storing the files.
+// 2. git init
+// 3. .git and it's imported because it handles the communication with git.
+// 4. No but it creates the folder that gives instructions to git
+// 5. You can run it at any stage
+
+// Ex 2
+// 1. Write the file in the working directory >>> Move the file into the staging area with git add >>> Commit the changes with git commit.
+// 2. git init >>> git remote >>> git add . >>> git commit -m "Added about.html".
+// 3. in the staging area you handel what file will be included in the next commit meaning you will pass to the staging area any file you would like to include in next commit.
+// 4. You will lose the control on the commit history.
+
+// Ex 3
+// 1. git diff
+// 2. git log
+// 3. git diff a1b2c3d e4f5g6h
+// 4. git status - shows what file had changes and need to be uploaded to git and git log shows th history of the commits that were done on the system.
+// 5. No git diff shows the changes in the code and git log shows the commit history.
+
+// Ex 4
+// 1. node_modules it's a big folder that holds all modules that the project holds and it could be very heavy and .env is's the file that has all the passwords and costume settings of the project.
+// 2. node_modules .env
+// 3. All of our password will be public
+// 4. No it will not remove it because .gitignore handles only new files not files that are in git already.
+
+// Ex 5
+// 1. git init >>> git remote
+// 2. git status
+// 3. echo "__pycache__\nsecrets.json" > .gitignore
+// 4. git add . >>> git commit -m "Added app.py"
+// 5. git diff >>> git add . >>> git commit -m "Added feature to app.py"
+// 6. git log
+// 7. git init >>> git add >>> git commit
+// 8. git commit
+// 9. It connects to are repository in github
